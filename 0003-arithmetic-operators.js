07f20399947b42c09e03fd5a94b6512942cad0d4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 11:55:16 +0300

    Create 0003-arithmetic-operators.js

---
+++ 0003-arithmetic-operators.js
@@ -0,0 +1,22 @@
+let x = 12
+
+let y = 7
+
+console.log(x + y)
+
+console.log(x - y)
+
+console.log(x * y)
+
+console.log(x / y)
+
+console.log((x / y).toFixed(2))
+
+console.log(x % y)
+
+// 19
+// 5
+// 84
+// 1.7142857142857142
+// 1.71
+// 5
