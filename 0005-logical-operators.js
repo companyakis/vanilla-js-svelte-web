1445b2d65fc0356555d3f07fcf02f6a330aa13d9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 12:06:33 +0300

    Create 0005-logical-operators.js

---
+++ 0005-logical-operators.js
@@ -0,0 +1,16 @@
+let x = true
+
+let y = false
+
+console.log(x && y)
+
+console.log(x || y)
+
+console.log(!x)
+
+console.log(!y)
+
+// main.jsx:5 false
+// main.jsx:7 true
+// main.jsx:9 false
+// main.jsx:11 true
