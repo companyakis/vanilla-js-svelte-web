f9a93c0e292c6d53c216342141cec930d59d4639
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 13:03:45 +0300

    Create 0007-type-conversion1.js

---
+++ 0007-type-conversion1.js
@@ -0,0 +1,16 @@
+let x = "123.456"
+
+let y = Number(x)
+
+let z = parseInt(x)
+
+let q = parseFloat(x)
+
+console.log(y)
+console.log(z)
+console.log(q)
+
+
+// main.jsx:9 123.456
+// main.jsx:10 123
+// main.jsx:11 123.456
