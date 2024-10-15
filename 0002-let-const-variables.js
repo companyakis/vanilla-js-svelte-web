7e9be042e922ebf6530c04ed7611dd23e51b4f38
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 11:47:06 +0300

    Create 0002-let-const-variables.js

---
+++ 0002-let-const-variables.js
@@ -0,0 +1,27 @@
+let name = "Hakan"
+
+let age = 17
+
+age = 27
+
+let pi = 3.14
+
+let ready = true
+
+let completed = false
+
+const birthYear = 1990
+
+//birthYear = 1989 // Uncaught TypeError: Assignment to constant variable.
+
+console.log(name)
+
+console.log(age)
+
+console.log(pi)
+
+console.log(ready)
+
+console.log(completed)
+
+console.log(birthYear)
