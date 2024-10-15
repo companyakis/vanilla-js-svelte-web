db4d581415f78da36a4c88301972da139471146b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 12:02:21 +0300

    Create 0004-comparision-operators.js

---
+++ 0004-comparision-operators.js
@@ -0,0 +1,27 @@
+let x = 12
+
+let y = "12"
+
+let z = 7
+
+console.log(x == y)
+
+console.log(x === y)
+
+console.log(x != y)
+
+console.log(x !== y)
+
+console.log(x == z)
+
+console.log(x > z)
+
+console.log(z <= x)
+
+// main.jsx:7 true
+// main.jsx:9 false
+// main.jsx:11 false
+// main.jsx:13 true
+// main.jsx:15 false
+// main.jsx:17 true
+// main.jsx:19 true
