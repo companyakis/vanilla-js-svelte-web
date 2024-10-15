cf798effb7633ce603243339303b123b74e0360b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-15 13:11:52 +0300

    Create 0007-type-conversion3.js

---
+++ 0007-type-conversion3.js
@@ -0,0 +1,29 @@
+let x = true
+
+let y = false
+
+let a = Number(x)
+
+let b = Number(y)
+
+console.log(a)
+
+console.log(b)
+
+let c = x.toString()
+
+console.log(typeof c)
+
+let name1 = "Aygün"
+
+let name2 = ""
+
+console.log(Boolean(name1))
+
+console.log(Boolean(name2))
+
+// main.jsx:9 1
+// main.jsx:11 0
+// main.jsx:15 string
+// main.jsx:21 true
+// main.jsx:23 false
