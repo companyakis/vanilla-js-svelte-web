3fc96b2df8c6dc1964e9469e782e8d18c87a01fe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-29 19:40:29 +0300

    Create 0009-string-methods.js

---
+++ 0009-string-methods.js
@@ -0,0 +1,26 @@
+let myName = "mustafa büyükdereli"
+
+console.log("Name variable length: " + myName.length)
+
+console.log(myName[0])
+
+console.log(myName.toLocaleLowerCase())
+
+console.log(myName.toUpperCase())
+
+console.log(myName.indexOf("b"))
+
+console.log(myName.lastIndexOf("ü"))
+
+console.log(myName.substring(0,3))
+
+console.log(myName.replace("m", "n"))
+
+// Name variable length: 19
+// main.jsx:5 m
+// main.jsx:7 mustafa büyükdereli
+// main.jsx:9 MUSTAFA BÜYÜKDERELI
+// main.jsx:11 8
+// main.jsx:13 11
+// main.jsx:15 mus
+// main.jsx:17 nustafa büyükderel
