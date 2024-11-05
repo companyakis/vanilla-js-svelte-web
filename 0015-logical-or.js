a324bb25dc4c58c0cf5ecee86ec796b0c401190d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-05 18:45:21 +0300

    Create 0015-logical-or.js

---
+++ 0015-logical-or.js
@@ -0,0 +1,13 @@
+let person1 = "Ayhan"
+
+let person2 = "AYGUN"
+
+if (person1.toLowerCase() === "mustafa" || person2.toLowerCase() === "aygun") {
+
+    console.log("You can enter the system...")
+}
+
+else {
+
+    console.log("Try again...")
+}
