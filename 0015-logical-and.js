96774971cbbd582f1c0110c1553db9152ab2ef27
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-05 18:42:27 +0300

    Create 0015-logical-and.js

---
+++ 0015-logical-and.js
@@ -0,0 +1,13 @@
+let userName = "Mustafa"
+
+let password = "Qwer12346"
+
+if (userName.toLowerCase() === "mustafa" && password === "Qwer1234") {
+
+    console.log("You can enter the system...")
+}
+
+else {
+
+    console.log("Try again...")
+}
