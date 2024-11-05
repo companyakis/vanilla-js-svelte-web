fa4b2064a65c193f126586c14955156b38bceaf2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-05 18:36:19 +0300

    Create 0014-conditional.js

---
+++ 0014-conditional.js
@@ -0,0 +1,21 @@
+let counter = "wwww"
+
+if (counter < 11) {
+
+    console.log("Not enough!")
+} 
+
+else if (counter === 11) {
+
+    console.log("Shouldn't be equal to 11")
+}
+
+else if (counter > 11) {
+
+    console.log("Yes!")
+}
+
+else {
+
+    console.log("Use a numeric value or try again...")
+}
