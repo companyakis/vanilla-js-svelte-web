16f892dd476f3d252ab7e39921e2ae624517ac0a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-07 18:21:48 +0300

    Create 0020-function-parameters.js

---
+++ 0020-function-parameters.js
@@ -0,0 +1,22 @@
+function greeting(name) {
+
+    console.log("Hi " + name +"!")
+}
+
+greeting("Mustafa")
+
+greeting("Aygün")
+
+const sayHi = function(userName) {
+
+    console.log(`Hello ${userName}!`)
+}
+
+sayHi("Ayhan")
+
+sayHi("Bengü")
+
+// Hi Mustafa!
+// Hi Aygün!
+// Hello Ayhan!
+// Hello Bengü!
