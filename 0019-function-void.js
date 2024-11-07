ff4d1846cf782fb896673da3c1fb70a1e28fb4bb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-07 18:17:41 +0300

    Create 0019-function-void.js

---
+++ 0019-function-void.js
@@ -0,0 +1,22 @@
+function greeting() {
+
+    console.log("Hi!")
+}
+
+greeting()
+
+greeting()
+
+const sayHi = function() {
+
+    console.log("Hi there!")
+}
+
+sayHi()
+
+sayHi()
+
+// Hi!
+// Hi!
+// Hi there!
+// Hi there!
