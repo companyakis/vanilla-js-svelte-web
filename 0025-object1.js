b220b2342029c3d657382f704cd58fc2c6191d86
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-08 14:30:28 +0300

    Create 0025-object1.js

---
+++ 0025-object1.js
@@ -0,0 +1,26 @@
+let company = {
+
+    name: "Ceteris Paribus Inc",
+    founder: "Lorem Ipsum",
+    foundationYear: 2024,
+    employees: 145
+}
+
+console.log(company.name)
+
+company.name = "CP Inc"
+
+console.log(company.name)
+
+console.log(company)
+
+// Ceteris Paribus Inc
+
+// CP Inc
+
+// {
+//   name: 'CP Inc',
+//   founder: 'Lorem Ipsum',   
+//   foundationYear: 2024,     
+//   employees: 145
+// }
