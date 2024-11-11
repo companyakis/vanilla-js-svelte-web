b72ae024e23b2b319f659507b4b5e2b9fb9d979b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-11 22:30:07 +0300

    Create 0027-query-selector2-main.js

---
+++ 0027-query-selector2-main.js
@@ -0,0 +1,15 @@
+const pFirst = document.querySelector('p')
+
+//console.log(pFirst) //  <p>Year 2024</p>
+
+const classProverbElem = document.querySelector(".proverb")
+
+//console.log(classProverbElem) // <p class="proverb">A rolling stone gathers no moss</p>
+
+const classWarningElem = document.querySelector(".warning")
+
+//console.log(classWarningElem) // <p class="warning">Study hard!</p>
+
+const divClassWarningElem = document.querySelector("div.warning")
+
+//console.log(divClassWarningElem) // <div class="warning">Don't waste your time!</div>
