0e878450cea4654ef69fd2768614ea069d07360d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-11 21:15:44 +0300

    Create 0026-math2.js

---
+++ 0026-math2.js
@@ -0,0 +1,16 @@
+let randomNum = Math.random()
+
+console.log(randomNum)
+
+let randomInteger = Math.round(Math.random() * 100)
+
+console.log(randomInteger)
+
+// 0.9357416779848136
+// 15
+
+// 0.09831882466910957
+// 7
+
+// 0.2417206006909458
+// 3
