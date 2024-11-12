d4abc96fe7c6ea11410be94d090af80bebdd6c39
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-12 22:49:10 +0300

    Create 0047-reverse1.js

---
+++ 0047-reverse1.js
@@ -0,0 +1,18 @@
+let names = ["yazgül", "bengü", "hakan", "kağan"]
+
+names.reverse()
+
+console.log(names)
+
+const points = [700, 75, 250, 3, 10, 800, 270]
+
+points.reverse()
+
+console.log(points)
+
+// [ 'kağan', 'hakan', 'bengü', 'yazgül' ]
+  
+// [
+//   270, 800,  10, 3,
+//   250,  75, 700
+// ]
