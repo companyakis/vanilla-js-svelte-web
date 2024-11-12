a3ee250cc45464fcbeb03d7341136107b2cf069d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-12 13:50:50 +0300

    Create 0044-push-pop.js

---
+++ 0044-push-pop.js
@@ -0,0 +1,25 @@
+let birth_years = [1948, 1956, 1981, 1991, 2014, 2017]
+
+birth_years.push(2000)
+
+birth_years.push(2001)
+
+birth_years.push(2002)
+
+console.log(birth_years)
+
+birth_years.pop()
+
+console.log(birth_years)
+
+// [
+//     1948, 1956, 1981,
+//     1991, 2014, 2017,
+//     2000, 2001, 2002
+//   ]
+
+//   [
+//     1948, 1956, 1981,
+//     1991, 2014, 2017,
+//     2000, 2001
+//   ]
