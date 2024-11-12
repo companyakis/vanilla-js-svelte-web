34f048f1d805d690ea345b120e1ca7b4c011dea8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-12 22:45:58 +0300

    Create 0046-sort2.js

---
+++ 0046-sort2.js
@@ -0,0 +1,25 @@
+let numbers = [1, 10, 32, 24, 27, 45, 17]
+
+let sorted = numbers.sort()
+
+console.log(sorted) // [1, 10, 17, 24, 27, 32, 45]
+
+const points = [700, 75, 250, 3, 10, 800, 270]
+
+let sorted_points = points.sort()
+
+console.log(sorted_points)
+
+// be careful!
+
+// [
+//     1, 10, 17, 24,
+//    27, 32, 45     
+//  ]
+
+//  [
+//     10, 250, 270, 3,
+//    700,  75, 800
+//  ]
+
+
