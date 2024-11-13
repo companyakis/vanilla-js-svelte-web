9bec23c2ef3991f16b12c0a1bcf5e58b41c99082
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-13 14:32:18 +0300

    Create 0049-date-time3.js

---
+++ 0049-date-time3.js
@@ -0,0 +1,11 @@
+let timeStamp1 = 1731497119064
+
+console.log(new Date(timeStamp1))
+
+let timeStamp2 = 1331496119064
+
+console.log(new Date(timeStamp2))
+
+// 2024-11-13T11:25:19.064Z
+
+// 2012-03-11T20:01:59.064Z
