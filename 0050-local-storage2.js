e8e62a6083d66dc30315f1e0670b01bcf538ed35
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-14 10:16:53 +0300

    Create 0050-local-storage2.js

---
+++ 0050-local-storage2.js
@@ -0,0 +1,17 @@
+window.localStorage.setItem("founder", "Mustafa Büyükdereli")
+
+window.localStorage.setItem("visionStatement", "A rolling stone gathers no moss")
+
+// update data
+
+localStorage.setItem("founder", "Ceteris Paribus")
+
+localStorage.setItem("visionStatement", "Lorem Ipsum")
+
+let ourFounder = window.localStorage.getItem("founder")
+
+let ourVision = window.localStorage.getItem("visionStatement")
+
+console.log(ourFounder)
+
+console.log(ourVision)
