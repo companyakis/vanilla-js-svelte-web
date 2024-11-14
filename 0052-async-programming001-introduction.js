6a6c157c332b4783c34c5d213b6e90219ead2d1b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-14 23:01:33 +0300

    Create 0052-async-programming001-introduction.js

---
+++ 0052-async-programming001-introduction.js
@@ -0,0 +1,26 @@
+console.log(1);
+
+console.log(2);
+
+console.log(3);
+
+console.log(4);
+
+console.log(5);
+
+setTimeout(() => {
+  console.log(6);
+}, 3000);
+
+console.log(7);
+
+console.log(8);
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 7
+// 8
+// 6
