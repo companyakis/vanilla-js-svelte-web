5df7fb651389c8d920adc821fce922c17bbc4cc1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-18 23:09:49 +0300

    Create 0028-document-methods002-byclassname-main.js

---
+++ 0028-document-methods002-byclassname-main.js
@@ -0,0 +1,12 @@
+let byClass = document.getElementsByClassName("warning")
+
+console.log(byClass.length)
+
+console.log(byClass[0])
+
+console.log(byClass[1])
+
+
+// main.js:5 2
+// main.js:7 <p class=​"warning">​Study hard!​</p>​
+// main.js:9 <div class=​"warning">​Don't waste your time!​</div>​
