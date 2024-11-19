31966af5dff17fd475a34fa0abc9c41e4f61135c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-19 12:21:35 +0300

    Create 0028-document-methods005-innertext1.js

---
+++ 0028-document-methods005-innertext1.js
@@ -0,0 +1,7 @@
+let titleValue = document.querySelector('title').innerText 
+
+console.log(titleValue) // Mustafa Buyukdereli was here
+
+let h2Value = document.querySelector("h2").innerText
+
+console.log(h2Value) // My name is Mustafa
