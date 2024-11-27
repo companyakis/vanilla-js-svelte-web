f72d5bc087c31b258e577f892546afe3b05a79f4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-27 16:48:09 +0300

    Create 0500-alchemy-university0009-AddTwo.js

---
+++ 0500-alchemy-university0009-AddTwo.js
@@ -0,0 +1,9 @@
+//  Your Goal: Complete the addTwo function
+// Complete the addTwo function to take an input and add 2 to it.
+// Return that sum.
+
+function addTwo(a) {
+    return a + 2
+}
+
+module.exports = addTwo;
