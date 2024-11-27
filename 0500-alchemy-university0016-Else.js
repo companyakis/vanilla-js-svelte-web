cb16d2a3a7d06c09579d1b133dd7d94785de7e52
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-27 16:59:06 +0300

    Create 0500-alchemy-university0016-Else.js

---
+++ 0500-alchemy-university0016-Else.js
@@ -0,0 +1,14 @@
+//  Your Goal: Not Equals Or Else
+// Let's update our isNotEqual function to also handle the case where a is equal to b.
+
+// If a is not equal to b return true. Otherwise, return false.
+
+function isNotEqual(a, b) {
+
+    if (a !== b) { return true }
+
+    else { return false }
+    
+}
+
+module.exports = isNotEqual;
