2e2c5fc031f90443c26d35c9d37f6ede7b190427
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-27 16:49:31 +0300

    Create 0500-alchemy-university0010-Multiply.js

---
+++ 0500-alchemy-university0010-Multiply.js
@@ -0,0 +1,9 @@
+// Your Goal: Multiply Two Numbers
+// Take two inputs in our product function and multiply them together using the * operator. Return this value.
+
+function product(x, y) {
+    
+    return  x * y
+}
+
+module.exports = product;
