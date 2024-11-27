3da42e5cff136160d70332651a95bdacc1bfcd7a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-27 16:51:03 +0300

    Create 0500-alchemy-university0011-Average.js

---
+++ 0500-alchemy-university0011-Average.js
@@ -0,0 +1,10 @@
+// Your Goal: Complete the average function
+// Taking what you learned above, find the average of four numerical values a, b, c, and d.
+// Once you have found the average, be sure to return it.
+
+function average(a, b, c, d) {
+    
+    return (a + b + c + d) / 4
+}
+
+module.exports = average;
