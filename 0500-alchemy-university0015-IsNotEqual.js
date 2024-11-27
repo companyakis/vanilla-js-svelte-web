5f0e4126e93c45f60a89599b1f265b002f01418d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-27 16:57:20 +0300

    Create 0500-alchemy-university0015-IsNotEqual.js

---
+++ 0500-alchemy-university0015-IsNotEqual.js
@@ -0,0 +1,12 @@
+//  Your Goal: Check Equal
+// Let's complete the isNotEqual function!
+
+// If a is not equal to b return true.
+
+function isNotEqual(a, b) {
+
+    if (a !== b) { return true }
+    
+}
+
+module.exports = isNotEqual;
