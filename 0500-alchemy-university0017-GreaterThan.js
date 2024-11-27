bbe6951a89ee929645557a655181a351e8599d0a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-27 17:02:46 +0300

    Create 0500-alchemy-university0017-GreaterThan.js

---
+++ 0500-alchemy-university0017-GreaterThan.js
@@ -0,0 +1,17 @@
+// Your Goal: Which is Greater?
+// Given two arguments, first and last, determine which one is greater.
+// If first is greater than last, return first!
+// If last is greater than first, return last!
+// If they are equal, do not return anything.
+
+function greater(first, last) {
+
+    if (first > last) { return first }
+
+    else if (last > first ) { return last} 
+
+    else { return null }
+    
+}
+
+module.exports = greater;
