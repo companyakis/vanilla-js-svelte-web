4d07904c122a6977f3d72cd77876b3162fcdb3ca
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-27 16:46:54 +0300

    Create 0500-alchemy-university0008-GetMessage.js

---
+++ 0500-alchemy-university0008-GetMessage.js
@@ -0,0 +1,11 @@
+// Your Goal: Return a string from getMessage
+// In the getMessage function return a string. It can be any string that you choose!
+// Once you have done this, click "Run Tests" or hit the enter key to move forward!
+
+function getMessage() {
+
+    return "Mustafa Büyükdereli"
+     
+}
+
+module.exports = getMessage;
