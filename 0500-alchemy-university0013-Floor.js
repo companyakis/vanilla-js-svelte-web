8ae344b509bd519d85a4c9fe94fa29b7ef9b7e38
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-27 16:54:11 +0300

    Create 0500-alchemy-university0013-Floor.js

---
+++ 0500-alchemy-university0013-Floor.js
@@ -0,0 +1,11 @@
+// Your Goal: Complete the getFloor function
+// Take the argument x and use Math.floor to turn it into an integer without the values after the decimal place.
+// Once you have this floored value, return it!
+
+function getFloor(x) {
+
+    return Math.floor(x)
+    
+}
+
+module.exports = getFloor;
