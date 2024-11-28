37abbd6ebfa18300d255ac04ac98afef3f29585b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-28 14:39:05 +0300

    Create 0500-alchemy-university0030-StartsWithX.js

---
+++ 0500-alchemy-university0030-StartsWithX.js
@@ -0,0 +1,8 @@
+//  Your Goal: Complete the Function
+// Complete the startsWithX function to determine if the first character of the string argument is the lower-case x.
+// If the first character is x return true. If not, return false.
+
+function startsWithX(string) {
+    
+    return string[0].toLowerCase() === "x"
+}
