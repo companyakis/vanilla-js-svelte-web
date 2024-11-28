83d3365cbd11c8aa8fce84f5b5902d16b588606e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-28 14:03:20 +0300

    Create 0500-alchemy-university0025-isEven.js

---
+++ 0500-alchemy-university0025-isEven.js
@@ -0,0 +1,10 @@
+//  Your Goal: Is It Even?
+// Given an integer value num, determine if it is even.
+// If it is even, return true. Return false otherwise.
+
+function isEven(num) {
+
+    return num % 2 === 0
+}
+
+module.exports = isEven;
