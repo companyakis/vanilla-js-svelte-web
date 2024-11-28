4030891e46827870d810e928cb0a66a9823ad8c7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-28 14:00:04 +0300

    Create 0500-alchemy-university0024-TopDouble.js

---
+++ 0500-alchemy-university0024-TopDouble.js
@@ -0,0 +1,14 @@
+// Your Goal: Complete the Top Double
+// Using whichever loop you'd like, complete the top double function to find the largest double for the value that is below the top.
+
+function topDouble(value, top) {
+
+
+    while (value < top) {
+
+        value *= 2
+    }
+
+    return value / 2
+    
+}
