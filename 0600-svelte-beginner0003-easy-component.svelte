3484f3dfa90d35c70ce87af896ae0abeaf4bc1ad
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-29 14:05:45 +0300

    Create 0600-svelte-beginner0003-easy-component.svelte

---
+++ 0600-svelte-beginner0003-easy-component.svelte
@@ -0,0 +1,15 @@
+<script>
+	let myName = "Mustafa Buyukdereli"
+
+	let years = [2024, 2025]
+
+	let iSay = "A rolling stone gathers no moss..."
+</script>
+
+<h2>My name is {myName}</h2>
+
+<h3>When Svelte: from {years[0]} to {years[1]}...</h3>
+
+<h4>I want to say {iSay}</h4>
+
+
