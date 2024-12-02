3d581f9f7a5d7118a558746f16399349b6540130
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-02 11:20:58 +0300

    Create 0500-alchemy-university0041-PizzaOrder.js

---
+++ 0500-alchemy-university0041-PizzaOrder.js
@@ -0,0 +1,16 @@
+//  Your Goal: Create a Pizza Order
+// Let's create an object representing a pizza order! 
+
+// In the order object, add the following three keys with values accordingly:
+
+// pizzas - Any number greater than zero.
+// extraCheese - A boolean. Either true or false.
+// deliveryInstructions - Any string of instructions.
+
+const order = {
+
+    pizzas: 3,
+    extraCheese: true,
+    deliveryInstructions: "In 30 minutes!"
+    
+};
