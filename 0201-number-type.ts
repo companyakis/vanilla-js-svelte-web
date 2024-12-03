b7dc57f074fae4dc9e4870879462270c555163f2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-03 20:22:55 +0300

    Create 0201-number-type.ts

---
+++ 0201-number-type.ts
@@ -0,0 +1,15 @@
+let year: number = 2024
+
+//year = "This year" // main.ts:3:1 - error TS2322: Type 'string' is not assignable to type 'number'.
+
+let x: number
+
+let y: number
+
+x = 12
+
+y = 23.34 // float
+
+let sum = x + y
+
+console.log(`${x} + ${y} = ${sum}`) // 12 + 23.34 = 35.34
