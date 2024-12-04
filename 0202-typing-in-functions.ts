881145601a6a4e8d99905ce5c20a46e46124eaeb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-04 13:51:05 +0300

    Create 0202-typing-in-functions.ts

---
+++ 0202-typing-in-functions.ts
@@ -0,0 +1,15 @@
+function sum(x: number, y: number, z: number) :  number {
+
+    return x + y + z
+
+}
+
+let result = sum(3, 4, 6)
+
+console.log(result) // 13
+
+const add = (x: number, y: number, z: number) => x + y + z
+
+let result2 = add(5, 2.34, 10)
+
+console.log(result2) // 17.34
